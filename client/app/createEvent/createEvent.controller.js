2d8293258ea993dbcfd19409cfe5dac3053b0b1f
Stephen Chan <stmchan@ucdavis.edu>
2014-07-22 08:38:24 -0700

    "Added in some geolocation modifications to createevent.js"

--- client/app/createEvent/createEvent.controller.js
+++ client/app/createEvent/createEvent.controller.js
@@ -2,15 +2,24 @@
 
 angular.module('goApp')
   .controller('CreateEventCtrl', function ($scope, $http, $location) {
-
+    $scope.location = "";
+    $scope.location2 = "";
+    $scope.dummyLocation = "";
     $scope.eventObj = {
       'startDate': "",
       'endDate': "",
       'eventLocation': "",
       'eventName': ""
     };
-
     $scope.create = function(){
+      console.log("$scope.location:", $scope.location);
+
+      console.log("$scope.dummy", $scope.dummyLocation);
+      if($scope.location === '' || $scope.location2 === ''){
+        alert('Directive did not update the location property in parent controller.');
+      } else {
+        alert('Yay. Location: ' + $scope.eventObj.eventLocation);
+      }
       $http.post('/api/events', $scope.eventObj).
       success(function(data){
         $location.path('/');
@@ -40,7 +49,30 @@ $scope.endTime = {
 $scope.locationName = {
   "title": "Location",
   "content": "e.g. 123 Madeup Street"
-}
+};
 
-});
+}) //Controller
+  .directive('googlePlaces', function(){
+    return {
+      restrict:'E',
+      replace:true,
+      // transclude:true,
+      scope: {location:'='},
+      template: '<input id="google_places_ac" name="google_places_ac" type="text" class="input-block-level form-control"/>',
+      link: function($scope, elm, attrs){
+        var autocomplete = new google.maps.places.Autocomplete($("#google_places_ac")[0], {});
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+          var place = autocomplete.getPlace();
+
+          var location = place.geometry.location.lat();
+          var location2 = place.geometry.location.lng();
+          $scope.$apply(function(){
+            $scope.location = location;
+            $scope.location2 = location2;
+          }); 
+        });
+
+      }
+    }
+  });
 
--- client/app/findEvent/findEvent.controller.js
+++ client/app/findEvent/findEvent.controller.js
@@ -11,21 +11,4 @@ angular.module('goApp')
         alert('Yay. Location: ' + $scope.location);
       }
     };
-  })
-  .directive('googlePlaces', function(){
-    return {
-      restrict:'E',
-      replace:true,
-      // transclude:true,
-      scope: {location:'='},
-      template: '<input id="google_places_ac" name="google_places_ac" type="text" class="input-block-level form-control"/>',
-      link: function($scope, elm, attrs){
-        var autocomplete = new google.maps.places.Autocomplete($("#google_places_ac")[0], {});
-        google.maps.event.addListener(autocomplete, 'place_changed', function() {
-          var place = autocomplete.getPlace();
-          $scope.location = place.geometry.location.lat() + ',' + place.geometry.location.lng();
-          $scope.$apply();
-        });
-      }
-    }
   });
